4d75e41fa1c0e6768070c0eca16bdad5c8774946
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2019-09-21 07:00:34 +0700

    add order route

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const user = require('./user');
 const product = require('./product');
 const cart = require('./cart');
+const order = require('./order');
 
 router.use('/user', user);
 router.use('/product', product);
 router.use('/cart', cart);
+router.use('/order', order);
 
 module.exports = router;
